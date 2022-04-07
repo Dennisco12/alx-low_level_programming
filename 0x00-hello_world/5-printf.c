8ea3fb45067ab5bf43ae81060abf0b583c2e676f
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-07 14:00:46 -0700

    Printing with only printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Here I want to print with only
+ * printf, without using puts
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
