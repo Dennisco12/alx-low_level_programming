09632c711e05be48fc664a7f50568524d61b2b5b
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-11 13:19:48 +0100

    prints all the digits in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	int d ='0';
+	int e = 'a';
+	
+	for (d = 0; d <= 9; d++)
+	putchar ('0' + d);
+
+	for (e = 0; e <= 5; e++)
+	putchar ('a' + e);
+
+	putchar ('\n');
+		return (0);
+}
