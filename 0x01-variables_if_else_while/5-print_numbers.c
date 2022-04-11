11a2cda4c5302196a114e42a4f7957a4befbadb7
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-11 05:14:09 +0100

    Prints all digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point;
+ *
+ * description - prints all the numbers in base 10
+ * return - 0
+ */
+
+int main (void)
+{
+	int d;
+	printf("0123456789\n");
+		return (0);
+}
