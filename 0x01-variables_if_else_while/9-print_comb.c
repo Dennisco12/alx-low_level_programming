5ebfcc2a79dacdc8a867e95683a644fdea2590fb
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-11 13:58:07 +0100

    print all combinations of a single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,13 +4,15 @@
 
 /** main - this is the entry point
  *
- * description - prints all possible combination of a single digit
+ * description - prints all possible combination of a si
+ * ngle digit
  * return - always (0)
  */
 
 int main(void)
 {
 	int d = 0;
+
 	while (d < 10)
 
 	{
