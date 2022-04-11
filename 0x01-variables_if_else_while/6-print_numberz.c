b0ab1a5677aeb71ae894e0a5257fb7682934b9ea
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-11 08:49:04 +0100

    prints digits using only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - universal entry points
+ *
+ * description - prints all digits in base 10 using only the putchar function.
+ * return - Always 0
+ */
+
+int main(void)
+
+{
+	char d = '0';
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	putchar('\n');
+	return (0);
+}
