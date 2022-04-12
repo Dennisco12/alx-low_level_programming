70dbceb8d66ba85cb3683260b0cb61f60a045735
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-12 06:09:53 -0700

    Started the first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description - prints the string putchar
+ * return - always 0
+ */
+
+int main(void);
+{
+	putchar("_putchar"):
+		return (0);
+}
