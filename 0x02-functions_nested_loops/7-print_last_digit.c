5c490749fe45aa51bdad1c1bde98b5e8190470ec
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-13 17:05:16 +0100

    print the last digit of an input in absolute

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * description: function that prints the last digit of an input
+ * @n: input value
+ * Return: always 0
+ */
+
+int print_last_digit(int n)
+{
+	int d;
+	if (n < 0)
+	{
+		d = (-1 * (% n))
+			_putchar (d + '0');
+			return (0);
+	}
+
+	else
+	{
+		d = (% n)
+			_putchar (d + '0');
+		return (0);
+	}
+}
