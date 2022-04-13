e363274d2a9b7ff8ad4dcc8072f9b80cd1baf40f
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-13 21:13:34 +0100

    count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+include "main.h"
+
+/**
+ * print_to_98 - entry point
+ * @n: input value
+ * Return: always 0
+ */
+
+int print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--);
+		{
+			printf("d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
