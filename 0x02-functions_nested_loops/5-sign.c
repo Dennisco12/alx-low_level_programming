e939373683b47fc9ba440659549668cbdaae0772
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-13 14:38:19 +0100

    print abs value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_sign - entry point
- * @n: input value
- * Return: 1 if positive, 0 if zero and -1 if negative
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ * the int n to print
+ *
+ * Return: always 0
  */
-
 int print_sign(int n)
+
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar ('+');
 	return (1);
 	}
-	else if (n < '0')
+	else if (n < 0)
 	{
 		_putchar ('-');
 	return (-1);
