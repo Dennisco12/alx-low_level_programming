0de498b1c1dafb2fa099829d5aebd061c22b75d8
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-13 21:29:13 +0100

    print from input to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,15 @@
  * Return: always 0
  */
 
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	int i;
 
 	if (n > 98)
 	{
-		for (i = n; i > 97; i--);
+		for (i = n; i > 97; i--)
 		{
-			printf("d", i);
+			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
@@ -26,7 +26,7 @@ int print_to_98(int n)
 	{
 		for (i = n; i < 99; i++)
 		{
-			printf("d", i);
+			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
