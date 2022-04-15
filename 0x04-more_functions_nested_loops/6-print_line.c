38f9572aa7b7b028729eba4c01acd2c2bbd5bb39
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-15 05:24:17 +0100

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: input value
+ * return: always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar ('_');
+		}
+	}
+	_putchar ('\n');
+}
