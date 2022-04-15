0ccd08216c6fef3dc8cc330a91356dc7866c706a
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-14 19:46:56 -0700

    old

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_sign - entry point
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (n > '0')
 	{
 		_putchar ('+');
-	return (1);
+		return (1);
 	}
 	else if (n < '0')
 	{
 		_putchar ('-');
-	return (-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar ('0');
-	return (0);
+		return (0);
 	}
 }
