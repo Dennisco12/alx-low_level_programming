66abccb3600f5bc2cd7ce58a1e06d1fc051f427f
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-22 06:13:01 -0700

    Updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,21 @@
 
 /**
  * _strcpy - function that copies a string to a buffer
+ * @dest: input string
+ * @src: input string
  * return: 0
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int i;
 
-	return (*dest);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
