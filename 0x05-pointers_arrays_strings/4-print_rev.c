779ed1bf9a5d5027df187c50d333be5d9e1ccfb7
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-22 02:24:58 +0100

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,7 @@
 
 /**
  * print_rev - function that prints the reverse of a string
- * @i: final order of elements
  * @s: input string
- * @n: intial order of elements
  * Return: 0
  */
 
@@ -12,12 +10,15 @@ void print_rev(char *s)
 {
 	int i, n;
 
-	n = 0;
-	while (s[n])
-		n++;
+	for (n = 0; s[n] != '\n'; n++)
 	{
-		for (i = n; i >= 0; i--)
-			_putchar (s[i]);
+		continue;
+	}
+
+	i = n - 1;
+	for (i = n; i >= 0; i--)
+	{
+		_putchar (s[i]);
 	}
 	_putchar ('\n');
 	return;
