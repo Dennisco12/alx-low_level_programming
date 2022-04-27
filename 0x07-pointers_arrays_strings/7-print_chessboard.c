39a2bb7e41a810c8aaee441d18d79ea177842283
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-27 02:25:54 +0100

    update

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * print_chessboard - A functiin that prints a chessboard
+ * @a: a double array
+ */
+
+void print_chessboard(char (*a)[8])
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * peint_diagsums - prints sum of diagonals of a square matrix
+ * print_diagsums - prints sum of diagonals of a square matrix
  * @a: the matrix
  * @size: length of matrix
  * Return: sum
@@ -9,10 +9,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, j, sum1, sum2;
 
-	i = j = 0;
+	size = 0;
 	while (a[size][size])
 	{
 		size++;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			sum1 = 0;
+			sum2 = 0;
+			if (i = j)
+				sum1  = sum1 + a[i][j];
+		}
 	}
