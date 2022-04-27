19d36a3aa28df999d67cd867a9e728de5c864663
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-04-26 17:55:33 -0700

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
 
 #endif
