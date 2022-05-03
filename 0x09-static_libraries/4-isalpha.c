247a851539afa50690936a74858f63c26b56879b
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-05-03 12:50:58 +0100

    updated

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"                                       
+/**
+ * _isalpha - all entry point
+ * @l: input value
+ * Return: 1 if value is a char, return 0 if otherwise
+ */
+
+int _isalpha(int l)
+{
+        if (((l >= 'a') && (l <= 'z')) || ((l >= 'A') && (l <= 'Z')))
+                return (1);
+        else
+                return (0);
+}
