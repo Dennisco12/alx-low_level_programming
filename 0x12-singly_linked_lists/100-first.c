5b4a160986bae3b8959fc31b972d5e2fdaa20b21
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-05-26 10:06:15 -0700

    updated

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * bmain - a function that is exected before main
+ */
+
+void _attribute_((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+The is the first set of tasks for singly linked lists
