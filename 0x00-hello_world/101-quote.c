9c2e057bd20eef1657a266dfcd20edf81a247690
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-06-17 04:29:26 +0100

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+#include <unistd.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * description - prints a quote
- * return - 0 (success)
+ * Return: Always 1 (Success)
  */
-
-int main(void);
+int main(void)
 {
-	fprintf ('and that piece of art is useful\" - Dora Korpar, 2015-10-19\n');
-	return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
